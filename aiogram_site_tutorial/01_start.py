85d51ffb6fd7d3585a4320491d49353d5a49bcc1
evgeniy <pavlinbl4@gmail.com>
2024-04-06 22:47:50 +0300

    aiogram tutorial - filter users

--- aiogram_site_tutorial/01_start.py
+++ aiogram_site_tutorial/01_start.py
@@ -1,9 +1,9 @@
 import asyncio
 import logging
 import sys
-from os import getenv
 
-from aiogram import Bot, Dispatcher, Router, types
+
+from aiogram import Bot, Dispatcher, Router, types, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
@@ -18,6 +18,9 @@ TOKEN = Credentials().pavlinbl4_bot
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 
+# Define a list of allowed user IDs
+# allowed_user_ids = {123456789, 987654321, 1237220337}
+
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
@@ -29,23 +32,31 @@ async def command_start_handler(message: Message) -> None:
     # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
     # method automatically or call API method directly via
     # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
-    await message.answer(f"Your id, {hbold(message.from_user.id)}!")
+    # await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+    # await message.answer(f"Your id, {hbold(message.from_user.id)}!")
 
 
-@dp.message()
-async def echo_handler(message: types.Message) -> None:
+@dp.message(F.from_user.id.in_({187597961}))
+async def handle_allowed_user_messages(message: types.Message):
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}\n"
+                         f"you are allowed user!")
+
     """
     Handler will forward receive a message back to the sender
 
     By default, message handler will handle all message types (like a text, photo, sticker etc.)
     """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+    # try:
+    #     # Send a copy of the received message
+    #     await message.reply("Hello, allowed user!")
+    #     await message.send_copy(chat_id=message.chat.id)
+    # except TypeError:
+    #     # But not all the types is supported to be copied so need to handle it
+    #     await message.answer("Nice try!")
+@dp.message()
+async def handle_other_messages(message: types.Message):
+    # This function will be called for messages from any other user
+    await message.reply("Sorry, you are not an allowed user.")
 
 
 async def main() -> None:
