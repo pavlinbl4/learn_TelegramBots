bac42a508cd25905d65629fb3c72d2f89de5fca4
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-05-13 23:35:36 +0300

    added new commands for image bot

--- Aiogram/common/bot_commands_list.py
+++ Aiogram/common/bot_commands_list.py
@@ -1,8 +1,15 @@
-# from aiogram.types import BotCommand
-#
+from aiogram.types import BotCommand
+
 #
 # private = [
 #     BotCommand(command='menu', description="look at me"),
 #     BotCommand(command='about', description='Sad story'),
 #     BotCommand(command='shipping',description='Fool on the hill')
 # ]
+
+kp_uploader = [
+    BotCommand(command='start', description="Запустить бота"),
+    BotCommand(command='help', description='Помощь'),
+    BotCommand(command='add_image',description='Добавить фото'),
+    BotCommand(command='cancel',description='Отмена')
+]
